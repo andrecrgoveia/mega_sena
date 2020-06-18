cf7f7b8c67999b7d57e9414e103c4d71e435e446
André Goveia <andrecrgoveia@gmail.com>
2020-06-18 19:37:27 -0300

    Creating the script

---
+++ mega_sena.py
@@ -0,0 +1,15 @@
+"""
+A simple program that generate 6 aleatory numbers.
+"""
+
+from random import randint
+
+# Mega Sena is the name of a lotery in brazil
+mega_sena = []
+
+# Creating a loop
+for i in range(1, 7):
+    mega_sena.append(randint(1, 60))
+
+# Show the results
+print(f'Your numbers are {mega_sena}, good lucky!')
