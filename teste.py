2a6fd053a480ee8c995aaaee135ea43e7c26bd09
André Goveia <andrecrgoveia@gmail.com>
2020-06-21 20:51:26 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,17 +1,12 @@
 import PySimpleGUI as sg
 
-sg.theme('DarkGreen')   # Add a touch of color
-# All the stuff inside your window.
-layout = [  [sg.Text('Some text on Row 1')],
-            [sg.Text('Enter something on Row 2'), sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
+sg.theme('DarkGreen')
+layout = [sg.Text('Enter something on Row 2'), sg.InputText()]
 
-# Create the Window
 window = sg.Window('Window Title', layout)
-# Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+    if event == sg.WIN_CLOSED or event == 'Cancel':
         break
     print('You entered ', values[0])
 
