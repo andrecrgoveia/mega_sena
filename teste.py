37e6552f238d465e3a8228ada03e53090b3db39a
André Goveia <andrecrgoveia@gmail.com>
2020-06-22 16:16:28 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,15 +1,25 @@
-import PySimpleGUI as sg
-
-sg.theme('DarkGreen')
-layout = [[sg.Text('Your typed chars appear here:'), sg.Text(size=(15,1), key='-OUTPUT-')],
-          [sg.Input(key='-IN-')],
-          [sg.Button('Show'), sg.Button('Exit')]]
-
-window = sg.Window('Window Title', layout)
-while True:
-    event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel':
+import PySimpleGUI as sg 
+   
+      
+sg.theme('BluePurple') 
+   
+layout = [[sg.Text('Your typed characters appear here:'), 
+           sg.Text(size=(15,1), key='-OUTPUT-')], 
+          [sg.Input(key='-IN-')], 
+          [sg.Button('Display'), sg.Button('Exit')]] 
+  
+window = sg.Window('Introduction', layout) 
+  
+while True: 
+    event, values = window.read() 
+    print(event, values) 
+      
+    if event in  (None, 'Exit'): 
         break
-    print('You entered ', values[0])
-
-window.close()
+      
+    if event == 'Display': 
+        # Update the "output" text element 
+        # to be the value of "input" element 
+        window['-OUTPUT-'].update(values['-IN-']) 
+  
+window.close() 
