7950429c207eec98da68f8ef1c9e81fe712f4ba4
André Goveia <andrecrgoveia@gmail.com>
2020-06-22 16:55:24 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,14 +1,11 @@
 import PySimpleGUI as sg
 from random import randint
 
-# Mega Sena is the name of a lotery in brazil
 mega_sena = []
 
-# Creating a loop
 for i in range(1, 7):
     mega_sena.append(randint(1, 60))
 
-# Show the results
 text = sg.PopupGetText('Qual loteria você quer?', 'LOTERIAS!')
 for i in range(1,1000):
     sg.OneLineProgressMeter('Calculando chances', i+1, 1000, ' ', 'Acessando banco de dados...')
