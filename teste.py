dd641cfb1109d07d2d6f9f642df7f9b4c03b8cae
André Goveia <andrecrgoveia@gmail.com>
2020-06-22 16:44:30 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -10,4 +10,6 @@ for i in range(1, 7):
 
 # Show the results
 text = sg.PopupGetText('Qual loteria você quer?', 'LOTERIAS!')
+for i in range(1,1000):
+    sg.OneLineProgressMeter('Calculando chances', i+1, 1000, ' ', 'Acessando banco de dados...')
 sg.Popup('Seus números da sorte são: ', f'{mega_sena}')
