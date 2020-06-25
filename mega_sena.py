5ca208ff1c7391acef736713e4b577e8012891ca
André Goveia <andrecrgoveia@gmail.com>
2020-06-24 21:05:33 -0300

    Update mega_sena.py

--- mega_sena.py
+++ mega_sena.py
@@ -1,18 +1,31 @@
-"""
-A simple program that generate 6 aleatory numbers.
-"""
-
+import PySimpleGUI as sg
 from random import randint
 
-# Mega Sena is the name of a lotery in brazil
 mega_sena = []
 
-# Creating a loop
 for i in range(1, 7):
     mega_sena.append(randint(1, 60))
 
-for numero in mega_sena:
-    print(numero, end=' ')
+text = sg.PopupGetText('Qual loteria você quer?', 'LOTERIAS!')
+# layout the Window
+layout = [[sg.Text('Calculando os melhores números')],
+          [sg.ProgressBar(5000, orientation='h', size=(20, 20), key='progbar')],
+          [sg.Cancel()]]
 
-# Show the results
-#print(f'Your numbers are {mega_sena}, good lucky!')
+# create the Window
+window = sg.Window('Progresso', layout)
+# loop that would normally do something useful
+for i in range(5000):
+    # check to see if the cancel button was clicked and exit loop if clicked
+    event, values = window.read(timeout=0)
+    if event == 'Cancel' or event == sg.WIN_CLOSED:
+        break
+        # update bar with loop value +1 so that bar eventually reaches the maximum
+    window['progbar'].update_bar(i + 1)
+# done with loop... need to destroy the window as it's still open
+window.close()
+#for i in range(1,1000):
+#    sg.OneLineProgressMeter('Calculando chances', i+1, 1000, ' ', 'Acessando banco de dados...')
+
+
+sg.Popup('Seus números da sorte são: ', f'{mega_sena}')
